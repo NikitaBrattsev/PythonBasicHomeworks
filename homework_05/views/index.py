fa303192ac164bb7da58237cc4699a6752564b44
Nikita Brattsev <nikita.brattsev@nordigy.ru>
2022-01-09 15:50:18 +0300

    homework_05 added templates index, about

--- homework_05/views/index.py
+++ homework_05/views/index.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 from flask.templating import render_template
 
-my_app = Blueprint("my_app",__name__)
+my_app = Blueprint("my_app", __name__)
 
 
 @my_app.route("/", endpoint="index")
 def get_index():
-    return render_template('base.html')
+    return render_template('index.html')
+
 
 @my_app.route("/about/", endpoint="about")
 def get_about():
-    return "about"
+    return render_template('about.html')
